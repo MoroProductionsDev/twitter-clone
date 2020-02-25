05ac9a4d117f2fbf4881c828dd055a7094dc6ef6
RAUL RIVERO RUBIO <5000928902@mwccpod10-06.local>
2020-02-24 19:16:28 -0800

    - Added a custom label that display the error in the login page when the user cannot authenticate. The logic is implemented when failure to login occur.

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    @IBOutlet weak var errorLabel: UILabel!
     let TWITTER_URL = "https://api.twitter.com/oauth/request_token"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.displayError(flag: false)
 
         // Do any additional setup after loading the view.
     }
@@ -23,8 +26,19 @@ class LoginViewController: UIViewController {
             self.performSegue(withIdentifier: "login_homepage", sender: self)
         },
            failure: { (Error) in
-            print("Could not logged in")
+            self.errorLabel.text = "Could not logged in"
+            self.displayError(flag: true)
+            // print("Could not logged in")
         })
+    }
+    
+    func displayError(flag: Bool) {
+        if (flag) {
+            self.errorLabel.alpha = 1.0
+        }
+        else {
+            self.errorLabel.alpha = 0.0
+        }
     }
     /*
     // MARK: - Navigation
