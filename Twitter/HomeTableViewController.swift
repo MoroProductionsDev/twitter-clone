1c772c747b8ef8ab4f8b04f24c56f7394387d491
RAUL RIVERO RUBIO <5000928902@mwccpod10-06.local>
2020-02-25 17:10:46 -0800

    - Created the cell that will use dequeueReusableCell and return the cell - Used a temporal integral literal for the numberOfSections and tableview to display the table in the app.

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -29,16 +29,22 @@ class HomeTableViewController: UITableViewController {
         // Go back to login page
         self.dismiss(animated: true, completion: nil)
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweet cell", for: indexPath)
+        return cell
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
     /*
